0623b3084d8764b26e6a2e15cce40d1827a97290
Augustus Flynn <huytung139@gmail.com>
2022-02-06 15:34:42 +0700

    fix ui

--- src/containers/auth/login.js
+++ src/containers/auth/login.js
@@ -88,89 +88,88 @@ export function Login() {
     }
 
     const onKeyDonw = (e) => {
-        if(e.keyCode === 13)
+        if (e.keyCode === 13)
             _onSignin()
     }
 
 
 
     return (
-        <div className="container">
-            <div className='bg' />
-            <div className='bg2' />
-            <div className="content-left" />
-            <div className="content-right">
-                <h3 className="title">Login</h3>
+        <div className='bg'>
+            <div className="container">
+                <div className="content-right">
+                    <h3 className="title">Login</h3>
 
-                <div className="form-group">
-                    <FormControl
-                        className="form-control"
-                        sx={{
-                            m: 1,
-                            width: '100%'
-                        }} variant="outlined">
-                        <InputLabel
-                            htmlFor="outlined-adornment-password"
-                        >Your email</InputLabel>
-                        <OutlinedInput
-                            className="placholder"
-                            id="outlined-adornment-password"
-                            value={values.email}
-                            onChange={handleChange('email')}
-                            label="Yourasdasdsadsdaa"
-                            onKeyDown={onKeyDonw}
-                        />
-                    </FormControl>
-                    <label className="err">{err.errEmail}</label>
+                    <div className="form-group">
+                        <FormControl
+                            className="form-control"
+                            sx={{
+                                m: 1,
+                                width: '100%'
+                            }} variant="outlined">
+                            <InputLabel
+                                htmlFor="outlined-adornment-password"
+                            >Your email</InputLabel>
+                            <OutlinedInput
+                                className="placholder"
+                                id="outlined-adornment-password"
+                                value={values.email}
+                                onChange={handleChange('email')}
+                                label="Yourasdasdsadsdaa"
+                                onKeyDown={onKeyDonw}
+                            />
+                        </FormControl>
+                        <label className="err">{err.errEmail}</label>
 
-                    <FormControl
-                        className="form-control"
-                        sx={{
-                            m: 1,
-                            width: '100%'
-                        }} variant="outlined">
-                        <InputLabel
-                            htmlFor="outlined-adornment-password"
-                        >Your password</InputLabel>
-                        <OutlinedInput
-                            className="placholder"
-                            id="outlined-adornment-password"
-                            type={showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            label="TypefdgfgfgYour password"
-                            onKeyDown={onKeyDonw}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-                    <label className="err">{err.errPassword}</label>
-                    
-                    <p className='hint'>*  Use Daisy Care account</p>
-                    <Button
-                        className="button"
-                        variant="contained"
-                        onClick={() => _onSignin()}
-                        style={{ backgroundColor: "#000333" }}
-                    >Sign in</Button>
-                    <p className="mobile">Try on mobile?</p>
-                    <div className="other-media">
-                        <div className="android-app" />
-                        <div className="ios-app" />
-                    </div>
+                        <FormControl
+                            className="form-control"
+                            sx={{
+                                m: 1,
+                                width: '100%'
+                            }} variant="outlined">
+                            <InputLabel
+                                htmlFor="outlined-adornment-password"
+                            >Your password</InputLabel>
+                            <OutlinedInput
+                                className="placholder"
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                onChange={handleChange('password')}
+                                label="TypefdgfgfgYour password"
+                                onKeyDown={onKeyDonw}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                        <label className="err">{err.errPassword}</label>
 
-                    <div className="forget">
-                        <Link to="/forgot-password">Forgot your password</Link>
+                        <p className='hint'>*  Use Daisy Care account</p>
+                        <Button
+                            className="button"
+                            variant="contained"
+                            onClick={() => _onSignin()}
+                            style={{ backgroundColor: "#000333" }}
+                        >Sign in</Button>
+                        <p className="mobile">Try on mobile?</p>
+                        <div className="other-media">
+                            <div className="ios-app" />
+                            <div onClick={() => window.open(process.env.REACT_APP_MOBILE_URL, "_blank")} className="android-app" />
+                        </div>
+
+                        {/* <div className="forget">
+                            <Link to="/forgot-password">Forgot your password</Link>
+                        </div> */}
                     </div>
                 </div>
             </div>
